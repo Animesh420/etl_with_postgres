d118a59367d23ea4d25721ba1421d41a597d3d9a
Animesh420 <ani420mkjee@gmail.com>
2021-12-28 11:08:56 +0530

    ETL| Putting primary keys and not null constraints along with on conflict behavior

--- sql_queries.py
+++ sql_queries.py
@@ -22,21 +22,21 @@ time_table_drop = prompt_delete.format(TIME)
 # CREATE TABLES
 
 songplay_table_create = (prompt_create.format(SONGPLAY) + """ (
-songplay_id serial,
-start_time timestamp,
-user_id int,
+songplay_id serial primary key,
+start_time timestamp not null,
+user_id int not null,
 level text,
-song_id text,
-artist_id text,
+song_id text not null,
+artist_id text not null,
 session_id text,
-location text,
+location text not null,
 user_agent text
 )
 """)
-
+̐
 user_table_create = (prompt_create.format(USERS) + """(
-user_id int not null unique,
-first_name text,
+user_id int not null unique primary key,
+first_name text not null,
 last_name text,
 gender text,
 level text
@@ -44,25 +44,25 @@ level text
 """)
 
 song_table_create = (prompt_create.format(SONGS) + """(
-song_id text not null unique,
-title text,
-artist_id text,
+song_id text not null unique primary key,
+title text not null,
+artist_id text not null,
 year int,
-duration numeric
+duration numeric not null
 )
 """)
 
 artist_table_create = (prompt_create.format(ARTISTS) + """(
-artist_id text not null unique,
-artist_name text,
-artist_location text,
-artist_latitude numeric,
-artist_longitude numeric
+artist_id text not null unique primary key,
+artist_name text not null,
+artist_location text not null,
+artist_latitude numeric not null,
+artist_longitude numeric not null
 )
 """)
 
 time_table_create = (prompt_create.format(TIME) + """(
-start_time timestamp,
+start_time timestamp primary key,
 hour int,
 day text,
 week int,
@@ -81,22 +81,40 @@ VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 
 user_table_insert = (prompt_insert.format(USERS) + """
 (user_id, first_name, last_name ,gender ,level )
-VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING
+VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id)
+    DO UPDATE
+    SET
+    first_name = EXCLUDED.first_name,
+    last_name = EXCLUDED.last_name,
+    gender = EXCLUDED.gender,
+    level = EXCLUDED.level
 """)
 
 song_table_insert = (prompt_insert.format(SONGS) + """
 (song_id, title, artist_id, year, duration)
-VALUES (%s, %s, %s, %s, %s)
+VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id)
+    DO UPDATE
+    SET
+    title = EXCLUDED.title,
+    artist_id = EXCLUDED.artist_id,
+    year = EXCLUDED.year,
+    duration = EXCLUDED.duration
 """)
 
 artist_table_insert = (prompt_insert.format(ARTISTS) + """
 (artist_id , artist_name , artist_location , artist_latitude , artist_longitude )
-VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING
+VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id)
+    DO UPDATE
+    SET
+    artist_name = EXCLUDED.artist_name,
+    artist_location = EXCLUDED.artist_location,
+    artist_latitude = EXCLUDED.artist_latitude,
+    artist_longitude = EXCLUDED.artist_longitude
 """)
 
 time_table_insert = (prompt_insert.format(TIME) + """
 (start_time ,hour ,day ,week ,month ,year ,weekday )
-VALUES (%s, %s, %s, %s, %s, %s, %s)
+VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING
 """)
 
 # FIND SONGS
